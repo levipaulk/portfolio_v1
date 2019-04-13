39bb5349870117893cecbacb6eda906114ac38b4
Levi Paulk <levipaulk@gmail.com>
2019-04-13 16:11:04 -0400

    touch up structure, font, and sizing

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
-function classToggle() {
+function navClassToggle() {
     const navs = document.querySelectorAll('.Navbar__Items');
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
 
-document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
+// function meClassToggle() {
+//     const mes = document.querySelectorAll('.me');
+//     mes.forEach(me => me.classList.toggle('hide-me'));
+// }
+
+document.querySelector('.Navbar__Link-toggle').addEventListener('click', navClassToggle);
+// document.querySelector('.bio-wrapper').addEventListener('mouseover', meClassToggle);
